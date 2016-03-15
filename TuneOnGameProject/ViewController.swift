a6c1a92d5a1d4f9f7326bad37333e12213794fb5
郭毓棠 <justin831201@gmail.com>
2016-03-15 16:18:47 +0800

    Initial Commit

---
+++ TuneOnGameProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuneOnGameProject
+//
+//  Created by JustinKuo on 2016/3/15.
+//  Copyright © 2016年 IM_IOS_CodingGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
