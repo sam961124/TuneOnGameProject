5f7291213a376688c5460f594fa80c4629792a53
sam961124 <sam961124@gmail.com>
2016-03-16 23:33:56 +0800

    add view controller

---
+++ TuneOnGameProject/State6_QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  State6_QuizViewController.h
+//  TuneOnGameProject
+//
+//  Created by 李尚恩 on 2016/3/16.
+//  Copyright © 2016年 IM_IOS_CodingGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface State6_QuizViewController : NSObject
+
+@end
---
+++ TuneOnGameProject/State6_QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  State6_QuizViewController.swift
+//  TuneOnGameProject
+//
+//  Created by 李尚恩 on 2016/3/16.
+//  Copyright © 2016年 IM_IOS_CodingGroup. All rights reserved.
+//
+
+import UIKit
+
+class State6_QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
