06704a25443ac15aecd5f6c8ac493d8d298784b9
sam961124 <sam961124@gmail.com>
2016-03-17 01:34:48 +0800

    top-bar

--- TuneOnGameProject/State1_SplashViewController.swift
+++ TuneOnGameProject/State1_SplashViewController.swift
@@ -9,10 +9,8 @@
 import UIKit
 
 class State1_SplashViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
