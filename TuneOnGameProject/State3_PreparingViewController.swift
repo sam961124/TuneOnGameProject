29a9ced5f261a8d589b6a78a7160af8e6e7e2a70
郭毓棠 <justin831201@gmail.com>
2016-03-16 20:19:08 +0800

    Create State0, State1, State3, State8

---
+++ TuneOnGameProject/State3_PreparingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  State3_PreparingViewController.swift
+//  TuneOnGameProject
+//
+//  Created by JustinKuo on 2016/3/16.
+//  Copyright © 2016年 IM_IOS_CodingGroup. All rights reserved.
+//
+
+import UIKit
+
+class State3_PreparingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
