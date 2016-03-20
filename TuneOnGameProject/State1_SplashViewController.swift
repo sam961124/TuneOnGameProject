802110687e989523bf45cbde1ed28ddf4498cbd8
justin831201 <justin831201@gmail.com>
2016-03-20 20:00:24 +0800

    Shadow, UIButton, UIImageView tap update

--- TuneOnGameProject/State1_SplashViewController.swift
+++ TuneOnGameProject/State1_SplashViewController.swift
@@ -18,22 +18,35 @@ class State1_SplashViewController: ViewController {
         
         //tune_on_title code from here
         var tune_on_title: UIImageView
+        let title_image: UIImage = UIImage(named: "title.png")!
         let Tap = UITapGestureRecognizer(target: self, action: Selector("Tap:"))
         Tap.numberOfTapsRequired = 1
-        tune_on_title = UIImageView(image: UIImage(named: "title.png"))
-        tune_on_title.frame.size.height = screen_height
+        tune_on_title = UIImageView(image: title_image)
         tune_on_title.frame.size.width = screen_width/2
+        tune_on_title.frame.size.height = (screen_width/2)*((title_image.size.height)/(title_image.size.width))
         tune_on_title.center = CGPointMake(screen_width/2, screen_height/2)
         tune_on_title.contentMode = UIViewContentMode.ScaleAspectFit
         tune_on_title.userInteractionEnabled = true
         tune_on_title.addGestureRecognizer(Tap)
         self.view.addSubview(tune_on_title)
+        //end here
+        
+        //welcome_back code from here
+//        let welcome_back: UITextView = UITextView()
+//        welcome_back.frame.size.height = screen_height/4
+//        welcome_back.frame.size.width = screen_width/4
+//        welcome_back.text = "歡迎回來"
+//        welcome_back.center = CGPointMake(screen_width/2, screen_height*3/4)
+//        welcome_back.textColor = UIColor.blackColor()
+//        self.view.addSubview(welcome_back)
+        //end here
+        
         // Do any additional setup after loading the view.
     }
 
     func Tap(image: UIImageView){
         let nextviewController:UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier("State3_PreparingViewController")
-        self.presentViewController(nextviewController, animated: true, completion: nil)
+        self.presentViewController(nextviewController, animated: false, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
